21d5f8250e5cf10369a9315c5e3122049e322229
smuralidhar <smuralidhar@idiap.ch>
2017-02-19 16:42:45 +0100

    Test Changes
    Added a comment in audiovisual_stream file

--- audiovisual_stream.py
+++ audiovisual_stream.py
@@ -1,3 +1,4 @@
+#--- Import libraries
 import auditory_stream
 import chainer
 import visual_stream
@@ -10,14 +11,14 @@ class ResNet18(chainer.Chain):
             vis = visual_stream.ResNet18(),
             fc = chainer.links.Linear(512, 5, initialW = chainer.initializers.HeNormal())
         )
-    
+
     def __call__(self, x):
         h = [self.aud(True, chainer.Variable(chainer.cuda.to_gpu(x[0]), True)), chainer.functions.expand_dims(chainer.functions.sum(self.vis(True, chainer.Variable(chainer.cuda.to_gpu(x[1][:256]), True)), 0), 0)]
-        
+
         for i in xrange(256, x[1].shape[0], 256):
             h[1] += chainer.functions.expand_dims(chainer.functions.sum(self.vis(True, chainer.Variable(chainer.cuda.to_gpu(x[1][i : i + 256]), True)), 0), 0)
-        
+
         h[1] /= x[1].shape[0]
-        
+
         return chainer.cuda.to_cpu(((chainer.functions.tanh(self.fc(chainer.functions.concat(h))) + 1) / 2).data[0])
 ### MODEL ###
